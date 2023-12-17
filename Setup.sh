37792ecc5ab91f77eb586a8bf2f255485d2c97b7
NotDev <christiankilduff@gmail.com>
2023-12-17 08:42:28 -0500

    setup

--- Setup.sh
+++ Setup.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Store the provided directory in a variable
+# Store the provided directory in a vººariable
 target_directory="$1"
 
 # Check if the provided path is a valid directory
